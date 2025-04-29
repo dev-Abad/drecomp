a4952446e2b2507457e22b837e748c060f6fa280
Astraaaal <gelogonzales321@gmail.com>
2025-04-29 15:35:47 +0800

    sixth commit

--- resources/views/printer.blade.php
+++ resources/views/printer.blade.php
@@ -1,191 +1,202 @@
 @extends('index')
 @section('title', 'PRINTER')
 @section('content')
-<h2 class="text-center fw-bold my-5" style="background-color: blue; color: white; padding: 10px;">STANDARD RYZEN PACKAGE</h2>
-<div class="text-center fw-bold my-5" color: white; padding: 10px;>
-    <h2 >FREEBIES: </h2>
-    <p>SECURE AVR, INPLAY H20 GAMING HEADSET, PATCH CABLE 10M CAT5, MOUSEPAD NON-STITCHED</p>
-</div>
+<h2 class="text-center fw-bold my-5" style="background-color: blue; color: white; padding: 10px;">PRINTERS</h2>
+
 <section class="bg-light py-3 d-flex justify-content-around flex-wrap gap-3">
     <div class="card" style="width: 20rem;">
-        <img src="{{ asset('image/desktop/a8.png') }}" class="card-img-top" alt="...">
+        <img src="{{ asset('image/printer/t420w.png') }}" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">
                 <h3 class="fw-bold">SPECIFICATION</h3>
-                <strong>PROCESSOR</strong>:A8-7680<br>
-                <strong>MOTHERBOARD</strong>:FM2 MOBO<br>
-                <strong>RAM</strong>:8GB 1666MHZ DDR3<br>
-                <strong>SSD</strong>:128GB SATA<br>
-                <strong>CASING</strong>:ORDINARY CASING<br>
-                <strong>PSU</strong>:700W<br>
-                <strong>MONITOR</strong>:19" LED MONITOR<br>
-                <strong>KB/MOUSE</strong>:INPLAY STX360 GAMING COMBO<br><br>
+                <li>A4 print speed up to 16 ipm (Black)/ 9 ipm (Colour)</li>
+                <li>WiFi, Mopria, Wireless Direct, USB 2.0</li>
+                <li>Full ink bottles included. Yield up to 7500/5000 pages (black/colour)*</li>
+                <li>Compatible with Mobile Connect app</li>
                 <h3 class="fw-bold" style="color: blue">PHP 8,699</h3>
-                <strong>CPU ONLY</strong>= PHP 5,699<br>
             </p>
         </div>
     </div>
     <div class="card" style="width: 20rem;">
-        <img src="{{ asset('image/desktop/a6.png') }}" class="card-img-top" alt="...">
+        <img src="{{ asset('image/printer/t426w.png') }}" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">
             <h3 class="fw-bold">SPECIFICATION</h3>
-                <strong>PROCESSOR</strong>:A6-9500<br>
-                <strong>MOTHERBOARD</strong>:B450MHP MOBO<br>
-                <strong>RAM</strong>:8GB 2666MHZ DDR4<br>
-                <strong>SSD</strong>:128GB SATA<br>
-                <strong>CASING</strong>:ORDINARY CASING<br>
-                <strong>PSU</strong>:700W<br>
-                <strong>MONITOR</strong>:19" LED MONITOR<br>
-                <strong>KB/MOUSE</strong>:INPLAY STX360 GAMING COMBO<br><br>
-                <h3 class="fw-bold" style="color: blue">PHP 11,799</h3>
-                <strong>CPU ONLY</strong>= PHP 8,399<br>
+            <li>A4 print speed up to 16 ipm (Black)/ 9 ipm (Colour)</li>
+                <li>WiFi, Mopria, Wireless Direct, USB 2.0</li>
+                <li>Full ink bottles included. Yield up to 7500/5000 pages (black/colour)*</li>
+                <li>Compatible with Mobile Connect app</li>
+                <h3 class="fw-bold" style="color: blue">PHP 8,699</h3>
+                
             </p>
         </div>
     </div>
     <div class="card" style="width: 20rem;">
-        <img src="{{ asset('image/desktop/athlon.png') }}" class="card-img-top" alt="...">
+        <img src="{{ asset('image/printer/t520w.png') }}" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">
             <h3 class="fw-bold">SPECIFICATION</h3>
-                <strong>PROCESSOR</strong>:ATHLON 300GE<br>
-                <strong>MOTHERBOARD</strong>:B450MHP MOBO<br>
-                <strong>RAM</strong>:8GB 2666MHZ DDR4<br>
-                <strong>SSD</strong>:256GB SATA<br>
-                <strong>CASING</strong>:ORDINARY CASING<br>
-                <strong>PSU</strong>:700W<br>
-                <strong>MONITOR</strong>:19" LED MONITOR<br>
-                <strong>KB/MOUSE</strong>:INPLAY STX360 GAMING COMBO<br><br>
+                <li>A4 print speed up to 17 ipm (Black)/ 9.5 ipm (Colour)</li>
+                <li>WiFi, AirPrint, Mopria, WiFi Direct, USB 2.0</li>
+                <li>1-line LCD</li>
+                <li>Full ink bottles included. Yield up to 7500/5000 pages (black/colour)*</li>
+                <li>Compatible with Mobile Connect app</li>
                 <h3 class="fw-bold" style="color: blue">PHP 12,299</h3>
-                <strong>CPU ONLY</strong>= PHP 9,105<br>
             </p>
         </div>
     </div>
     <div class="card" style="width: 20rem;">
-        <img src="{{ asset('image/desktop/ryzen3.png') }}" class="card-img-top" alt="...">
+        <img src="{{ asset('image/printer/t720dw.png') }}" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">
             <h3 class="fw-bold">SPECIFICATION</h3>
-                <strong>PROCESSOR</strong>:RYZEN 3 2200G<br>
-                <strong>MOTHERBOARD</strong>:B450MHP MOBO<br>
-                <strong>RAM</strong>:8GB 2666MHZ DDR4<br>
-                <strong>SSD</strong>:256GB SATA<br>
-                <strong>CASING</strong>:ORDINARY CASING<br>
-                <strong>PSU</strong>:700W<br>
-                <strong>MONITOR</strong>:19" LED MONITOR<br>
-                <strong>KB/MOUSE</strong>:INPLAY STX360 GAMING COMBOc<br><br>
+            <li>A4 print speed up to 17 ipm (Black)/ 16.5 ipm (Colour)</li>
+            <li>WiFi, AirPrint, Mopria, WiFi Direct, USB 2.0</li>
+            <li>Auto 2-sided (Duplex) printing, 20 sheets Auto-Document Feeder (ADF), 1 line LCD</li>
+            <li>Full ink bottles included. Yield up to 7500/5000 pages (black/colour)*</li>
+            <li>Compatible with Mobile Connect app</li>
                 <h3 class="fw-bold" style="color: blue">PHP 13,699</h3>
-                <strong>CPU ONLY</strong>= PHP 10,599<br>
             </p>
         </div>
     </div>
     <div class="card" style="width: 20rem;">
-        <img src="{{ asset('image/desktop/ryzen33.png') }}" class="card-img-top" alt="...">
+        <img src="{{ asset('image/printer/l121.png') }}" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">
             <h3 class="fw-bold">SPECIFICATION</h3>
-                <strong>PROCESSOR</strong>:RYZEN 3 3200G<br>
-                <strong>MOTHERBOARD</strong>:B450MHP MOBO<br>
-                <strong>RAM</strong>:8GB 2666MHZ DDR4<br>
-                <strong>SSD</strong>:256GB SATA<br>
-                <strong>CASING</strong>:ORDINARY CASING<br>
-                <strong>PSU</strong>:700W<br>
-                <strong>MONITOR</strong>:19" LED MONITOR<br>
-                <strong>KB/MOUSE</strong>:INPLAY STX360 GAMING COMBO<br><br>
+                <li>Print speed of up to 9.0 ipm (black) and 4.8 ipm (colour)</li>
+                <li>Ultra-high page yield of 4,500 pages (black) and 7,500 pages (colour)</li>
                 <h3 class="fw-bold" style="color: blue">PHP 15,399</h3>
-                <strong>CPU ONLY</strong>= PHP 12,099<br>
+                
             </p>
         </div>
     </div>
 </section>
 <section class="bg-light py-3 d-flex justify-content-around flex-wrap gap-3">
     <div class="card" style="width: 20rem;">
-        <img src="{{ asset('image/desktop/ryzen52.png') }}" class="card-img-top" alt="...">
+        <img src="{{ asset('image/printer/l3210.png') }}" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">
             <h3 class="fw-bold">SPECIFICATION</h3>
-                <strong>PROCESSOR</strong>:RYZEN 5 2400G<br>
-                <strong>MOTHERBOARD</strong>:B450MHP MOBO<br>
-                <strong>RAM</strong>:8GB 2666MHZ DDR4<br>
-                <strong>SSD</strong>:256GB SATA<br>
-                <strong>CASING</strong>:ORDINARY CASING<br>
-                <strong>PSU</strong>:700W<br>
-                <strong>MONITOR</strong>:19" LED MONITOR<br>
-                <strong>KB/MOUSE</strong>:INPLAY STX360 GAMING COMBO<br><br>
+                <li>Print, scan, copy</li>
+                <li>Compact integrated tank design</li>
+                <li>High yield ink bottles</li>
+                <li>Spill-free, error-free refilling</li>
+                <li>Borderless printing up to 4R</li>
                 <h3 class="fw-bold" style="color: blue">PHP 15,799</h3>
-                <strong>CPU ONLY</strong>= PHP 12,070<br>
             </p>
         </div>
     </div>
     <div class="card" style="width: 20rem;">
-        <img src="{{ asset('image/desktop/ryzen53.png') }}" class="card-img-top" alt="...">
+        <img src="{{ asset('image/printer/l3250.png') }}" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">
-            <h3 class="fw-bold">SPECIFICATION</h3>
-                <strong>PROCESSOR</strong>:RYZEN 5 3400G<br>
-                <strong>MOTHERBOARD</strong>:B450MHP MOBO<br>
-                <strong>RAM</strong>:8GB 2666MHZ DDR4<br>
-                <strong>SSD</strong>:256GB SATA<br>
-                <strong>CASING</strong>:ORDINARY CASING<br>
-                <strong>PSU</strong>:700W<br>
-                <strong>MONITOR</strong>:19" LED MONITOR<br>
-                <strong>KB/MOUSE</strong>:INPLAY STX360 GAMING COMBO<br><br>
+                <h3 class="fw-bold">SPECIFICATION</h3>
+                    <li>Print, scan, copy</li>
+                    <li>Compact integrated tank design</li>
+                    <li>High yield ink bottles</li>
+                    <li>Spill-free, error-free refilling</li>
+                    <li>Wi-Fi & Wi-Fi Direct</li>
+                    <li>Seamless setup via Epson Smart Panel</li>
+                    <li>Borderless printing up to 4R</li>
                 <h3 class="fw-bold" style="color: blue">PHP 14,799</h3>
-                <strong>CPU ONLY</strong>= PHP 11,399<br>
             </p>
         </div>
     </div>
     <div class="card" style="width: 20rem;">
-        <img src="{{ asset('image/desktop/ryzen54.png') }}" class="card-img-top" alt="...">
+        <img src="{{ asset('image/printer/l5290.png') }}" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">
             <h3 class="fw-bold">SPECIFICATION</h3>
-                <strong>PROCESSOR</strong>:RYZEN 5 4655G<br>
-                <strong>MOTHERBOARD</strong>:B450MHP MOBO<br>
-                <strong>RAM</strong>:8GB 2666MHZ DDR4<br>
-                <strong>SSD</strong>:256GB SATA<br>
-                <strong>CASING</strong>:ORDINARY CASING<br>
-                <strong>PSU</strong>:700W<br>
-                <strong>MONITOR</strong>:19" LED MONITOR<br>
-                <strong>KB/MOUSE</strong>:INPLAY STX360 GAMING COMBO<br><br>
+                <li>Print, Scan, Copy, Fax with ADF</li>
+                <li>Compact integrated tank design</li>
+                <li>High yield ink bottles</li>
+                <li>Spill-free, error-free refilling</li>
+                <li>Wi-Fi, Wi-Fi Direct</li>
+                <li>Epson Connect</li>
+                <li>Seamless setup via Epson Smart Panel</li>
+                <li>Borderless printing up to 4R</li>
                 <h3 class="fw-bold" style="color: blue">PHP 16,299</h3>
-                <strong>CPU ONLY</strong>= PHP 12,899<br>
             </p>
         </div>
     </div>
     <div class="card" style="width: 20rem;">
-        <img src="{{ asset('image/desktop/ryzen55.png') }}" class="card-img-top" alt="...">
+        <img src="{{ asset('image/printer/l8050.png') }}" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">
             <h3 class="fw-bold">SPECIFICATION</h3>
-                <strong>PROCESSOR</strong>:RYZEN 5 5600G<br>
-                <strong>MOTHERBOARD</strong>:B450MHP MOBO<br>
-                <strong>RAM</strong>:8GB 2666MHZ DDR4<br>
-                <strong>SSD</strong>:256GB SATA<br>
-                <strong>CASING</strong>:ORDINARY CASING<br>
-                <strong>PSU</strong>:700W<br>
-                <strong>MONITOR</strong>:19" LED MONITOR<br>
-                <strong>KB/MOUSE</strong>:INPLAY STX360 GAMING COMBO<br><br>
+            <li>Compact integrated tank design</li>
+            <li>Replaceable Maintenance Box</li>
+            <li>High yield ink bottles</li>
+            <li>Ultra-high page yield of 2,100 pages (colour)</li>
                 <h3 class="fw-bold" style="color: blue">PHP 18,999</h3>
-                <strong>CPU ONLY</strong>= PHP 15,999<br>
             </p>
         </div>
     </div>
     <div class="card" style="width: 20rem;">
-        <img src="{{ asset('image/desktop/ryzen7.png') }}" class="card-img-top" alt="...">
+        <img src="{{ asset('image/printer/l11050.png') }}" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">
             <h3 class="fw-bold">SPECIFICATION</h3>
-                <strong>PROCESSOR</strong>:RYZEN 7 5700G<br>
-                <strong>MOTHERBOARD</strong>:B450MHP MOBO<br>
-                <strong>RAM</strong>:8GB 2666MHZ DDR4<br>
-                <strong>SSD</strong>:256GB SATA<br>
-                <strong>CASING</strong>:ORDINARY CASING<br>
-                <strong>PSU</strong>:700W<br>
-                <strong>MONITOR</strong>:19" LED MONITOR<br>
-                <strong>KB/MOUSE</strong>:INPLAY STX360 GAMING COMBO<br><br>
+                <li>Compact and Durable Ecological Design</li>
+                <li>Print up to A3+</li>
+                <li>Easy operation with Epson Smart Panel version 4.3, with Print Head Cleaning notification</li>
+                <li>Replaceable Maintenance Box and Pick Up Roller</li>
+                <li>Ultra-high page yield of 7,000 pages (colour)</li>
+                <li>Easy operation with Epson Smart Panel</li>
                 <h3 class="fw-bold" style="color: blue">PHP 21,699</h3>
-                <strong>CPU ONLY</strong>= PHP 18,399<br>
+            </p>
+        </div>
+    </div>
+</section>
+<section class="bg-light py-3 d-flex justify-content-around flex-wrap gap-3">
+    <div class="card" style="width: 20rem;">
+        <img src="{{ asset('image/printer/g1010.png') }}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <p class="card-text">
+            <h3 class="fw-bold">SPECIFICATION</h3>
+                <li>Print Speed (A4, ISO): up to 8.8 / 5 ipm (mono/colour)</li>
+                <li>USB 2.0</li>
+                <li>Recommended Monthly Print Volume: 150 - 1,500 pages</li>
+                <h3 class="fw-bold" style="color: blue">PHP 15,799</h3>
+            </p>
+        </div>
+    </div>
+    <div class="card" style="width: 20rem;">
+        <img src="{{ asset('image/printer/g2020.png') }}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <p class="card-text">
+            <h3 class="fw-bold">SPECIFICATION</h3>
+                <li>Print, Scan, Copy</li>
+                <li>USB 2.0</li>
+                <li>Recommended Monthly Print Volume: 150 – 1,500 pages</li>
+                <li>Print Speed (A4, ISO): Up to 9.1 / 5 ipm (mono/colour)</li><br>
+                <h3 class="fw-bold" style="color: blue">PHP 14,799</h3>
+            </p>
+        </div>
+    </div>
+    <div class="card" style="width: 20rem;">
+        <img src="{{ asset('image/printer/g3010.png') }}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <p class="card-text">
+            <h3 class="fw-bold">SPECIFICATION</h3>
+                <li>Print, Scan, Copy</li>
+                <li>Print Speed (A4, ISO): up to 8.8 / 5 ipm (mono/colour)</li>
+                <li>Wi-Fi, AirPrint®, Mopria® Print Service, Direct Wireless</li>
+                <li>Recommended Monthly Print Volume: 150 - 1500 pages</li>
+                <h3 class="fw-bold" style="color: blue">PHP 16,299</h3>
+            </p>
+        </div>
+    </div>
+    <div class="card" style="width: 20rem;">
+        <img src="{{ asset('image/printer/hp115.png') }}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <p class="card-text">
+            <h3 class="fw-bold">SPECIFICATION</h3>
+                <li>A4 Color Inkjet Printer, Perfect for Home</li>
+                <li>Print only</li>
+                <li>Print speed up to 8 ppm (black) and 5 ppm (color)</li>
+                <h3 class="fw-bold" style="color: blue">PHP 18,999</h3>
             </p>
         </div>
     </div>
